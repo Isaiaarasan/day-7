845aadb694155a9dc9d607e888aa41796d4f726e
Isaiaarasan <arasan9706@gmail.com>
2025-01-24 09:38:22 +0530

    chNGES

--- day7/src/Components/FunctionalComponent/Navbar.jsx
+++ day7/src/Components/FunctionalComponent/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
-import '../../CSS/Navbar.css';
+import "../../css/Navbar.css";
 import { useState } from 'react';
 
 const Navbar = () => {
